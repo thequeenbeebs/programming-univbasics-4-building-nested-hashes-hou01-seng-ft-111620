e4422d9e8671e92ef67eb3a282d8992f252446eb
Blaire Baker <bakerblaire@gmail.com>
2020-10-23 16:48:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,8 @@ def monopoly_with_second_tier
   base_hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {},
+      :names => {}
     }
   }
 end
