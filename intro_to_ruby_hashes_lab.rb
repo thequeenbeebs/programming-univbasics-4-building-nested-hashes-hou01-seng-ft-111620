f0aaf1faeeaacd380ca8f39d6766261219e83646
Blaire Baker <bakerblaire@gmail.com>
2020-10-23 16:51:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,8 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {}
+        :reading_railroad => {},
+        :pennsylvania_railroad => {}
       }
     }
   }
