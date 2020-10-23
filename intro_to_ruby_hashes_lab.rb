ab2647aea56cd666783fa61e7bfb8cb9d61634e1
Blaire Baker <bakerblaire@gmail.com>
2020-10-23 16:48:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {
-    :railroads => {},
-    :pieces => 4
+    :railroads => {
+      :pieces => 4
+    }
   }
 end
 
